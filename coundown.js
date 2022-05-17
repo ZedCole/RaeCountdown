f633518278e47edea34695992ba4d62de4c6684f
ZedCole <50928706+ZedCole@users.noreply.github.com>
2022-05-17 21:43:11 +1000

    Added todo comment

--- coundown.js
+++ coundown.js
@@ -1,3 +1,8 @@
 var textBox = document.getElementById("text");
 var timer = document.getElementById("countdown");
 
+// To Do
+/*
+    - Create a Date function to get the date and check agianst 4/10/22
+    - Replace corrisponding text on page
+*/
